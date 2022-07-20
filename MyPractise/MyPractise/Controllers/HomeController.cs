e8d3d355ae92716fa14359c5f344295486ffcb98
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-07-20 12:02:15 -0500

    Adding My practise project

---
+++ MyPractise/MyPractise/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPractise.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            string todaysDate = DateTime.Now.ToString();
+            string message = "Welcome NAVEEN";
+            
+            // creating data for sending to the view
+
+            ViewBag.TodaysDate = $"Today's date is { todaysDate}";
+            ViewBag.Message = message;
+            return View();
+        }
+
+
+    }
+}
