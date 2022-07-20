de95bce0f8b621ca3b243c00aaa6bd7e195e67fe
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-07-20 16:59:05 -0500

    Age Calculator

---
+++ Age Calculator/practise2/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using practise2.Models;
+
+
+namespace practise2.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        
+
+        [HttpGet]  
+        public IActionResult Index()
+        {
+            return View(new Agemodel());
+        }
+        [HttpPost]
+        public IActionResult Index(Agemodel m)
+        {
+           
+            
+
+            return View("Hello", m);
+        }
+    }
+}
