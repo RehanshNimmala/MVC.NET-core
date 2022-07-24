41d180cc44adff4dc027e72e50f2d831c57eba7b
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-07-24 13:01:25 -0500

    Division calculation

---
+++ division calculation/AreaPractise/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using AreaPractise.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AreaPractise.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new Model());
+        }
+        [HttpPost]
+        public IActionResult Index(Model model)
+        {
+            return View("Result", model);
+        }
+
+
+    }
+}
