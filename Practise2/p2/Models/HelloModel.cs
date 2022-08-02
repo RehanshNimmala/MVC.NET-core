44f55edca152253be79b3422e0e4a42091c6f333
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-08-01 21:45:20 -0500

    add Practise2

---
+++ Practise2/p2/Models/HelloModel.cs
@@ -0,0 +1,19 @@
+﻿namespace p2.Models
+{
+    public class HelloModel
+    {
+       public string Name { get; set; }
+        public int Number { get; set; }
+
+        public int GetSquare()
+        {
+            return(int)Math.Pow(2,Number);
+        }
+        public int GetCube()
+        {
+            return(int)Math.Pow(3, Number);
+        }
+        
+
+    }
+}
