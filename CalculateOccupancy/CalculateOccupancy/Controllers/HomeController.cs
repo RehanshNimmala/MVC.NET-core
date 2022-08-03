c8ec43aef2531ce6c8db27a6e83a5df6dd607c49
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-08-03 13:07:01 -0500

    Occupancy calculation added

---
+++ CalculateOccupancy/CalculateOccupancy/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using CalculateOccupancy.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.ComponentModel.DataAnnotations;
+namespace CalculateOccupancy.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new Model());
+        }
+        [HttpPost]
+        public IActionResult Index(Model test)
+        {
+            return View("Results", test);
+        }
+
+
+      
+
+       
+    }
+}
