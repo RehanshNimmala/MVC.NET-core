696f5e58771fd02cf38bca8ce36d228905f6df50
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-08-03 21:15:05 -0500

    update occupancy calculation

---
+++ CalculateOccupancy/CalculateOccupancy/Controllers/BulkDensityController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace CalculateOccupancy.Controllers
+{
+    public class BulkDensityController : Controller
+        
+    {
+        [HttpPost]
+        public IActionResult Index(double weight,double volume)
+        {
+            double density = weight / volume;
+            ViewBag.Result= $"The Bulk density of the material is {density}";
+
+            return RedirectToAction("Index","Home",density);
+            //return View("Index");
+        }
+    }
+}
