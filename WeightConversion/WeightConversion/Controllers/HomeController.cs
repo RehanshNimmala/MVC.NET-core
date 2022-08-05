98feefb0c9e58d9209607d5cb56c12fc1201eb2d
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-08-04 21:41:00 -0500

    Weight conversion

---
+++ WeightConversion/WeightConversion/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WeightConversion.Models;
+
+namespace WeightConversion.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new UnitConversionModel());
+                
+        }
+        [HttpPost]
+        public IActionResult WeightConversion(UnitConversionModel model)
+        {
+            return View("Results",model);
+        }
+            
+        
+
+       
+    }
+}
