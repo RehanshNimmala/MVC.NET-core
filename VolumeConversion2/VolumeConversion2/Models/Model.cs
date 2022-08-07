e34d45959dfe0aff105693810ad4b67440aa9d78
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-08-07 18:45:18 -0500

    Liter to Gallon Conversion

---
+++ VolumeConversion2/VolumeConversion2/Models/Model.cs
@@ -0,0 +1,15 @@
+﻿namespace VolumeConversion2.Models
+{
+    public class Model
+    {
+        public double quantity { get; set; }
+
+        //public double volume { get; set; }
+
+        public double CalculateInGallon()
+        {
+            double gallon = quantity / 3.785;
+            return gallon;
+        }
+    }
+}
