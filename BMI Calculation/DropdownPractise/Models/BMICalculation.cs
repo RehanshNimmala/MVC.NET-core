b18e5e667ea38777180bac249bd0622c4cf10ad1
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-08-08 15:28:33 -0500

    BMI calculation-UI done

---
+++ BMI Calculation/DropdownPractise/Models/BMICalculation.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace DropdownPractise.Models
+{
+    public class BMICalculation
+    {
+        public string? Name { get; set; }    
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        public string? Gender { get; set; }
+
+        public List<SelectListItem> GenderList { get; set; } = new List<SelectListItem>()
+        {
+            new SelectListItem {Text="Male" },
+            new SelectListItem {Text="Female"}
+
+        };
+
+    }
+}
