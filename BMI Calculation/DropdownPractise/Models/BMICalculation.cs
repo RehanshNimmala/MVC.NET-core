bf20bb68e80dd6302a3e1fc901c458755abe54f9
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2022-08-09 20:31:06 -0500

    Updated

--- BMI Calculation/DropdownPractise/Models/BMICalculation.cs
+++ BMI Calculation/DropdownPractise/Models/BMICalculation.cs
@@ -14,16 +14,62 @@ namespace DropdownPractise.Models
         public List<SelectListItem> GenderList { get; set; } = new List<SelectListItem>()
         {
             new SelectListItem {Text="Male" },
-            new SelectListItem {Text="Female"}
-
+            new SelectListItem {Text="Female"} 
         };
+
+
+
+        public string? GetName()
+        {
+            if (Gender == "Male")
+            {
+                Name = "Mr." +""+ Name;
+            }
+            else if (Gender == "Female")
+            {
+                Name = "Mrs." +""+ Name;
+
+            }
+            return Name;
+        }
+
+
         public double CalculateBMI()
         {
-            double bmi = (Weight / Height/Height)* 10000;
-            double result=Math.Round(bmi,2);
-
+            //string? value = "";
+            double bmi = (Weight / Height / Height) * 10000;
+            double result = Math.Round(bmi, 2);
             return result;
-        }
+       }
+        //public string
+
+        //    if (result <= 18.5)
+        //    {
+        //        value = ("you're in the underweight range.");
+        //    }
+        //    else if (result > 18.5 & result < 24.9)
+        //    {
+
+        //        value = ("you're in the Normal range.");
+
+        //    }
+        //    else if (result > 24.9 & result < 29.9)
+        //    {
+
+        //        value = ("you're in the Over Weight range.");
+
+        //    }
+        //    else if (result > 30 & result < 39.9)
+        //    {
+
+        //        value = ("you're in the Obese range.");
+        //    }
+        //    return result;
+            
+        //}
+
+
+        
 
     }
 }
