ce1b30e8f50e8ca2023cde679b47dacf8b1e177e
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2023-01-12 22:40:48 -0600

    New project initiated

---
+++ MovieTicketsApp/MovieTicketsApp/Models/Categoty.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieTicketsApp.Models
+{
+    //1111111111111111111
+    public class Categoty
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+    }
+}
