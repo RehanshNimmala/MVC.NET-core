7a8e5029b25167660da2f341142b96759c2b0869
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2023-01-13 22:51:59 -0600

    event class added

---
+++ MovieTicketsApp/MovieTicketsApp/Models/Event.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieTicketsApp.Models
+{
+    public class Event
+    {
+        public int ID { get; set; }
+        public string? Title { get; set; }
+        public int CategoryID { get; set; }
+        public double TicketPrice { get; set; }
+        public string? Description { get; set; }
+        public string ImageID { get; set; }
+    }
+}
